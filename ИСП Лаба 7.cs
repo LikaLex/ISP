e0124c6a5dec2788f48cddb6df76a8d579fbf3b6
Любовь Онуфриюк <lika.lex555@gmail.com>
2017-08-03 19:00:01 +0300

    Add files via upload

---
+++ ИСП Лаба 7.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab7
+{
+    internal enum Type { Long, Double, Decimal, String }
+
+    internal class Rational // Этот класс должен содержать значение m / n, long m, n
+    {
+        // Поля и свойства
+        private long _m;
+        private long _n;
+        long m { get { return _m; } }
+        long n { get { return _n; } }
+
+
+        // методы
+        private static bool Cut(ref long a, ref long b)
+        {
+            bool flag = false;
+            long divider = 2;
+
+            while (divider <= a && divider <= b)
+            {
+                if (a % divider == 0 && b % divider == 0)
+                {
+                    a /= divider;
+                    b /= divider;
+                    flag = true;
+                }
+                else
+                    divider++;
+            }
+
+            return flag;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string ToString()
+        {
+            return m.ToString() + "/" + n.ToString();
+        }
+
+        public string ToString(Type arg)
+        {
+            if (arg == Type.String)
+                return arg.ToString();
+            else if (arg == Type.Long)
+                return ((long)m / n).ToString();
+            else if (arg == Type.Double)
+                return ((double)m / n).ToString();
+            else
+                return ((decimal)m / n).ToString();
+        }
+
+        // конструкторы
+        public Rational()
+        {
+            _m = 0;
+            _n = 1;
+        }
+
+        public Rational(long a, long b)
+        {
+            if (b <= 0)
+            {
+                Console.WriteLine("Inadequate data!");
+                Console.ReadKey(true);
+                Environment.Exit(1);
+            }
+            else
+            {
+                _m = a;
+                _n = b;
+            }
+            Cut(ref _m, ref _n);
+        }
+
+        public Rational(string arg)
+        {
+            string[] s = arg.Split('/');
+            _m = Convert.ToInt64(s[0]);
+            _n = Convert.ToInt64(s[1]);
+            Cut(ref _m, ref _n);
+        }
+
+        // Унарные операторы
+        public static Rational operator +(Rational arg)
+        {
+            return new Rational(Math.Abs(arg.m), arg.n);
+        }
+
+        public static Rational operator -(Rational arg)
+        {
+            return new Rational(-arg.m, arg.n);
+        }
+
+        // Операторы сравнения
+        public static bool operator ==(Rational arg1, Rational arg2)
+        {
+            if (arg1.m == arg2.m && arg1.n == arg2.n)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator !=(Rational arg1, Rational arg2)
+        {
+            if (arg1.m != arg2.m || arg1.n != arg2.n)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool operator <(Rational arg1, Rational arg2)
+        {
+            return (decimal)arg1.m / arg1.n < (decimal)arg2.m / arg2.n;
+        }
+
+        public static bool operator >(Rational arg1, Rational arg2)
+        {
+            return (decimal)arg1.m / arg1.n > (decimal)arg2.m / arg2.n;
+        }
+
+        // Арифметические операторы
+        public static Rational operator +(Rational arg1, Rational arg2)
+        {
+            long divided, divider;
+            checked
+            {
+                divided = arg1.m * arg2.n + arg2.m * arg1.n;
+                divider = arg1.n * arg2.n;
+            }
+            return new Rational(divided, divider);
+        }
+
+        public static Rational operator -(Rational arg1, Rational arg2)
+        {
+            return (arg1 + (-arg2));
+        }
+
+        public static Rational operator *(Rational arg1, Rational arg2)
+        {
+            long divided, divider;
+            checked
+            {
+                divided = arg1.m * arg2.m;
+                divider = arg1.n * arg2.n;
+            }
+            return new Rational(divided, divider);
+        }
+
+        public static Rational operator /(Rational arg1, Rational arg2)
+        {
+            long divided, divider;
+            checked
+            {
+                divided = arg1.m * arg2.n;
+                divider = arg1.n * arg2.m;
+                if (divided > 0 && divider > 0 || divided < 0 && divider < 0)
+                {
+                    divided = Math.Abs(divided);
+                    divider = Math.Abs(divider);
+                }
+                else
+                {
+                    divided = -Math.Abs(divided);
+                    divider = Math.Abs(divider);
+                }
+            }
+            return new Rational(divided, divider);
+        }
+
+        // Операторы преобразования
+        public static explicit operator long(Rational arg)
+        {
+            return arg.m / arg.n;
+        }
+
+        public static implicit operator double(Rational arg)
+        {
+            return (double)arg.m / arg.n;
+        }
+
+        public static implicit operator decimal(Rational arg)
+        {
+            return (decimal)arg.m / arg.n;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rational Rational1 = new Rational(5, 6);
+            Rational Rational2 = new Rational(1, 3);
+            Console.WriteLine("5/6 + 1/3 = " + (Rational1 + Rational2).ToString());
+            Console.WriteLine("5/6 - 1/3 = " + (Rational1 - Rational2).ToString());
+            Console.WriteLine("5/6 * 1/3 = " + (Rational1 * Rational2).ToString());
+            Console.WriteLine("5/6 / 1/3 = " + (Rational1 / Rational2).ToString());
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+
+            Rational1 = new Rational("230/240");
+            Rational2 = new Rational("240/230");
+            Console.WriteLine("230/240 + 240/230 = " + (Rational1 + Rational2).ToString());
+            Console.WriteLine("230/240 - 240/230 = " + (Rational1 - Rational2).ToString());
+            Console.WriteLine("230/240 * 240/230 = " + (Rational1 * Rational2).ToString());
+            Console.WriteLine("230/240 / 240/230 = " + (Rational1 / Rational2).ToString());
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+
+            Rational1 = new Rational(5, 6);
+            Rational2 = new Rational(1, 3);
+            Console.WriteLine("5/6 + 1/3 = " + (Rational1 + Rational2).ToString(Type.Decimal));
+            Console.WriteLine("5/6 - 1/3 = " + (Rational1 - Rational2).ToString(Type.Double));
+            Console.WriteLine("5/6 * 1/3 = " + (Rational1 * Rational2).ToString(Type.Long));
+            Console.WriteLine("5/6 / 1/3 = " + (Rational1 / Rational2).ToString(Type.String));
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+
+            Rational1 = new Rational("230/240");
+            Rational2 = new Rational("240/230");
+            Console.WriteLine("230/240 + 240/230 = " + (Rational1 + Rational2).ToString(Type.Decimal));
+            Console.WriteLine("230/240 - 240/230 = " + (Rational1 - Rational2).ToString(Type.Double));
+            Console.WriteLine("230/240 * 240/230 = " + (Rational1 * Rational2).ToString(Type.Long));
+            Console.WriteLine("230/240 / 240/230 = " + (Rational1 / Rational2).ToString(Type.String));
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ ИСП Лаба 8.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5
+{
+    class Program
+    {
+        internal delegate void EventHandler();
+        internal class TransportEvent
+        {
+            public event EventHandler Event;
+            public void GenerateEvent()
+            {
+                if (Event != null)
+                    Event();
+                else
+                    Console.WriteLine("An error occured. No event handlers found.");
+            }
+        }
+
+        internal class MeasurementInfo
+        {
+            private readonly string[] _measures = { "Km/h", "Kg", "Kwt", "Sec" };
+            public string[] Measures
+            {
+                get { return _measures; }
+            }
+
+            public string this[string index]
+            {
+                get
+                {
+                    if (index == "speed")
+                        return _measures[0];
+                    else if (index == "weight")
+                        return _measures[1];
+                    else if (index == "power")
+                        return _measures[2];
+                    else if (index == "time")
+                        return _measures[3];
+                    else
+                        return null;
+                }
+            }
+        }
+
+        internal enum Color
+        {
+            Red,
+            Green,
+            Blue,
+            Yellow,
+            Gray,
+            Black,
+            White,
+            Navi
+        }
+
+        internal class Transport
+        {
+            private static uint _numberOfObjects = 0;
+            public static uint NumberOfObjects
+            {
+                get { return _numberOfObjects; }
+            }
+
+            // Определение измерительной системы
+            protected MeasurementInfo _measurement;
+            public MeasurementInfo Measurement
+            {
+                get { return _measurement; }
+            }
+
+            protected uint _maxSpeed;
+            public uint MaxSpeed
+            {
+                get { return _maxSpeed; }
+            }
+
+            protected uint _weight;
+            public uint Weight
+            {
+                get { return _weight; }
+            }
+
+            protected uint _power;
+            public uint Power
+            {
+                get { return _power; }
+            }
+
+            protected double _timeTo100;
+            public double TimeTo100
+            {
+                get { return _timeTo100; }
+            }
+
+            //Пытаемся избежать неадекватных новых данных
+            public bool Modify(uint new_max_speed, uint new_weight, uint new_power, double new_timeTo100)
+            {
+                if (new_timeTo100 <= 0)
+                    return false;
+                else if (new_power * new_timeTo100 <= 5 * new_weight / 3.6 / 3.6)
+                    return false;
+                else
+                {
+                    _maxSpeed = new_max_speed;
+                    _weight = new_weight;
+                    _power = new_power;
+                    _timeTo100 = new_timeTo100;
+                    return true;
+                }
+            }
+
+            // Функции для отображения нашего объекта
+            public void Display()
+            {
+                Console.WriteLine(MaxSpeed.ToString() + " " + Measurement["speed"]);
+                Console.WriteLine(Power.ToString() + " " + Measurement["power"]);
+                Console.WriteLine(Weight.ToString() + " " + Measurement["weight"]);
+                Console.WriteLine(TimeTo100.ToString() + " " + Measurement["time"]);
+            }
+
+            public void Display(string divider)
+            {
+                Console.WriteLine(MaxSpeed.ToString() + divider + Measurement["speed"]);
+                Console.WriteLine(Power.ToString() + divider + Measurement["power"]);
+                Console.WriteLine(Weight.ToString() + divider + Measurement["weight"]);
+                Console.WriteLine(TimeTo100.ToString() + divider + Measurement["time"]);
+            }
+
+            // Определение конструкторов
+            public Transport()
+            {
+                _numberOfObjects++;
+                _maxSpeed = 0;
+                _weight = 0;
+                _power = 0;
+                _timeTo100 = 0;
+            }
+
+            public Transport(uint max_speed, uint weight, uint power, double timeTo100)
+            {
+                _numberOfObjects++;
+                if (timeTo100 <= 0 || (power * timeTo100 <= 5 * weight / 3.6 / 3.6))
+                {
+                    _maxSpeed = 0;
+                    _weight = 0;
+                    _power = 0;
+                    _timeTo100 = 0;
+                }
+                else
+                {
+                    _maxSpeed = max_speed;
+                    _weight = weight;
+                    _power = power;
+                    _timeTo100 = timeTo100;
+                }
+
+                _measurement = new MeasurementInfo();
+            }
+
+            ~Transport()
+            {
+                _numberOfObjects--;
+            }
+        }
+
+        internal abstract class Car : Transport
+        {
+            public abstract Color Color { get; }
+
+            public abstract ulong Price { get; }
+
+            public abstract bool SoldAlready { get; }
+
+            public abstract bool Sell();
+
+            public Car(uint max_speed, uint weight, uint power, double timeTo100) : base(max_speed, weight, power, timeTo100) { }
+        }
+
+        internal class FerrariEnzo : Car
+        {
+            private static uint _numberOfCars;
+            public static uint NumberOfCars
+            {
+                get { return _numberOfCars; }
+            }
+
+            private Color _color;
+            public override Color Color
+            {
+                get { return _color; }
+            }
+
+            private ulong _price;
+            public override ulong Price
+            {
+                get { return _price; }
+            }
+
+            private bool _sold;
+            public override bool SoldAlready
+            {
+                get { return _sold; }
+            }
+
+            public override bool Sell()
+            {
+                SellFerrari.GenerateEvent();
+                Console.WriteLine("We have sold our Ferrari for " + _price.ToString() + " dollars;");
+                _sold = true;
+                return true;
+            }
+
+            public TransportEvent SellFerrari;
+
+            public FerrariEnzo(Color FactoryColor) : base(350, 1255, 478, 3.3)
+            {
+                SellFerrari = new TransportEvent();
+                _sold = false;
+                _price = 650000;
+                _color = FactoryColor;
+                Console.WriteLine("We have produced " + Color.ToString() + " Enzo Ferrari.");
+                _numberOfCars++;
+            }
+
+            ~FerrariEnzo()
+            {
+                _numberOfCars--;
+            }
+        }
+
+        public static void SellFunction()
+        {
+            Console.WriteLine("Preparing for selling the car . . .");
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+        }
+
+        static void Main(string[] args)
+        {
+            // Отображение нашего транспортного объекта
+            Transport MyCar = new Transport(200, 1000, 200, 8);
+            MyCar.Display();
+            Transport[] MoreCars = { new Transport(250, 1200, 225, 7.4),
+                                     new Transport(250, 1200, 225, 7.4),
+                                     new Transport(250, 1200, 225, 7.4),
+                                     new Transport(250, 1200, 225, 7.4)
+                                   };
+            Console.WriteLine("We produced " + Transport.NumberOfObjects + " \"Transport\" objects");
+            Console.WriteLine();
+
+            // Показ наших объектов FerrariEnzo
+            FerrariEnzo MyFerrari = new FerrariEnzo(Color.Red);
+            FerrariEnzo[] MoreFerrari = { new FerrariEnzo(Color.Red),
+                                        new FerrariEnzo(Color.Yellow),
+                                        new FerrariEnzo(Color.Green),
+                                        new FerrariEnzo(Color.Yellow)
+                                      };
+            Console.WriteLine();
+
+            // Использование событий
+            MyFerrari.SellFerrari.Event += new EventHandler(SellFunction);
+            for (int i = 0; i < 4; i++)
+                MoreFerrari[i].SellFerrari.Event += new EventHandler(SellFunction);
+            Console.ReadKey(true);
+
+            // Использование статического метода
+            Console.WriteLine("We produced " + FerrariEnzo.NumberOfCars + " Enzo Ferrari.");
+            Console.WriteLine("The number of Transport objects and their descendants is " + Transport.NumberOfObjects.ToString());
+            Console.ReadKey(true);
+            Console.WriteLine();
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("The color of this Enzo Ferrari is " + MoreFerrari[i].Color.ToString());
+                MoreFerrari[i].Sell();
+                Console.WriteLine();
+            }
+
+            // Разрушение наших объектов
+            Console.WriteLine("Deleting our objects . . .");
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+
+            MyCar = null;
+            MyFerrari = null;
+            for (int i = 0; i < 4; i++)
+            {
+                MoreCars[i] = null;
+                MoreFerrari[i] = null;
+            }
+
+            // Попытка избежать потерь
+            Console.WriteLine();
+            GC.Collect(10, GCCollectionMode.Forced);
+            System.Threading.Thread.Sleep(2000);
+
+            Console.WriteLine("Now the number of our Ferrari is " + FerrariEnzo.NumberOfCars.ToString() + ";");
+            Console.WriteLine("The number of Transport objects and their descendants is " + Transport.NumberOfObjects.ToString());
+            Console.WriteLine("Press any key . . .");
+            Console.ReadKey(true);
+        }
+    }
+}
